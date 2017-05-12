3c84b5151b11ae7784f5e1bba3edf5d45543c23a
Jonathan Grim <jonjongrim@gmail.com>
2017-05-12 17:54:17 -0400

    Initiate npm package

--- puzzles.js
+++ puzzles.js
@@ -30,7 +30,7 @@ var puzzles = function () {
 
     class SentencePuzzle extends SimplePuzzle {
         constructor(hint, answer) {
-            super('Complete the sentence', hint, answer);
+            super('Complete the sentence / phrase', hint, answer);
         }
     }
 
@@ -40,12 +40,96 @@ var puzzles = function () {
         }
     }
 
+    class ActorPuzzle extends SimplePuzzle {
+        constructor(hint, answer) {
+            super('Name the actor / actress', hint, answer);
+        }
+    }
+
+    class ThingPuzzle extends SimplePuzzle {
+        constructor(hint, answer) {
+            super('Name the object', hint, answer);
+        }
+    }
+
+    class LocationPuzzle extends SimplePuzzle {
+        constructor(hint, answer) {
+            super('Name the location', hint, answer);
+        }
+    }
+
+    class TriviaPuzzle extends SimplePuzzle {
+        constructor(hint, answer) {
+            super('Answer the trivia question', hint, answer);
+        }
+    }
+
     var puzzles = [
         new SentencePuzzle('The only winning move is', 'not to play'),
         new CharacterPuzzle(
             "I'm a supercomputer who enjoys a nice game of chess",
             "WOPR"
         ),
+        new TriviaPuzzle(
+            "This government agency runs an operations center in the Cheyenne Mountain Complex",
+            "NORAD"
+        ),
+        new ActorPuzzle(
+            "I play David Lightman, a teenage hacker",
+            "Matthew Broderick"
+        ),
+        new ActorPuzzle(
+            "I play Jennifer Mack, a teenage girl who didn't know what to expect",
+            "Ally Sheedy"
+        ),
+        new TriviaPuzzle(
+            "David plays this classic game in the arcade",
+            "Galaga"
+        ),
+        new LocationPuzzle(
+            "This is David and Jennifer's hometown",
+            "Seattle"
+        ),
+        new LocationPuzzle(
+            "This is the first place chosen to be nuked",
+            "Las Vegas"
+        ),
+        new TriviaPuzzle(
+            "Randomly dialing numbers to discover computer connections is referred to as",
+            "War dialing"
+        ),
+        new CharacterPuzzle(
+            "This company creates 'the future' of computer games",
+            "Protovision"
+        ),
+        new TriviaPuzzle(
+            "What was the password to access WOPR?",
+            "Joshua"
+        ),
+        new SentencePuzzle(
+            "That system probably contains the new...",
+            "Data encryption algorithm"
+        ),
+        new TriviaPuzzle(
+            "What is Lightman told to look for to access the unidentified system?",
+            "Backdoor"
+        ),
+        new TriviaPuzzle(
+            "What was the first game on the list which helped Lightman discover the system's creator",
+            "Falken's Maze"
+        ),
+        new TriviaPuzzle(
+            "What game does Lightman choose to play with WOPR?",
+            "Global Thermonuclear War"
+        ),
+        new TriviaPuzzle(
+            "What side does Lightman choose in the game, Global Thermonuclear War?",
+            "USSR"
+        ),
+        new SentencePuzzle(
+            "What is the primary goal?",
+            "To win the game"
+        )
     ]
 
     function getPuzzle() {

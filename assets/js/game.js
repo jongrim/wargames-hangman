e61c17697b323a60fe1dfc41cd45aa6e0c506dd1
Jonathan Grim <jonjongrim@gmail.com>
2017-05-14 21:52:04 -0400

    Adds intial game js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,30 @@
 var puzzles = require('./puzzles.js');
 
-var game = {
-    puzzle: puzzles.getPuzzle(),
-    guessesRemaining: 6,
-    lettersGuessed: [],
-    puzzlePrompt: this.puzzle.prompt,
-    puzzleHint: this.puzzle.hint,
-    puzzleAnswer: this.puzzle.Answer,
-    newGuess: function (guess) {
+function Game () {
+    this.puzzle = puzzles.getPuzzle();
+    this.puzzlePrompt = puzzle.prompt;
+    this.puzzleHint = puzzle.hint;
+    this.puzzleAnswer = puzzle.answer;
+    this.guessesRemaining = 6;
+    this.lettersGuessed = [];
+    this.isOver = false;
+    this.addLetter = function (letter) {
+        this.lettersGuessed.push(letter);
+    }
+    this.findOccurences = function (letter) {
+        // Parameter: letter (string)
+        let occursAt = [];
+        if (this.puzzleAnswer.includes(letter)) {
+            // Find and return all occurences
+            let ans = this.puzzleAnswer;
+            for (var i = 0; i < ans; i++) {
+                if (ans[i] === letter) {
+                    occursAt.push(i);
+                }
+            }
+        }
+        return occursAt;
     }
 }
+
+exports.game = Game;
